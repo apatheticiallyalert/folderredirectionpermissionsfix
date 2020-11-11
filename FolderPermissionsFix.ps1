679d20ccd9acf71ff4789415376c0bba216327d5
cbrianfoley <cbrianfoley@users.noreply.github.com>
2020-11-11 11:19:17 -0600

    Update FolderPermissionsFix.ps1

--- FolderPermissionsFix.ps1
+++ FolderPermissionsFix.ps1
@@ -6,7 +6,7 @@ $dirs = Get-ChildItem $parentDir | Where-Object {$_.psiscontainer}
 
 foreach ($dir in $dirs){
     write-output $dir.fullname
-    takeown.exe /F $($dir.fullname) /R /D Y |out-null
+    takeown.exe /F $($dir.fullname) /R /D Y | Out-Null
     icacls.exe $($dir.fullname) /reset /T /C /L /Q
     icacls.exe $($dir.fullname) /grant ($($dir.basename) + ':(OI)(CI)F') /C /L /Q
     icacls.exe $($dir.fullname) /setowner $($dir.basename) /T /C /L /Q
